218c976f3be6c238e89b431f2c21c1a011bccdfe
DESKTOP-368J5V1\Anton <anton.aksyonov@cashforce.com>
2018-09-03 23:26:18 +0300

    Lesson 1 (added some functions)

--- lesson_2/functions.py
+++ lesson_2/functions.py
@@ -1,3 +1,44 @@
+# recursive function (factorial)
+def my_factorial_function(n):
+    if n == 0:
+        return 1
+    else:
+        return n * my_factorial_function(n - 1)
+
+
+print(f"factorial result: {my_factorial_function(10)}")
+
+
+# simple sort function
+def my_sort_function(sequence):
+    if type(sequence) is str:
+        print('this is string')
+        sequence = list(sequence)
+        for j in range(0, len(sequence)):
+            for i in range(0, len(sequence) - 1):
+                if sequence[i] > sequence[i + 1]:
+                    x = sequence[i + 1]
+                    sequence[i + 1] = sequence[i]
+                    sequence[i] = x
+        return sequence
+    if type(sequence) is list:
+        print('this is list')
+        for j in range(0, len(sequence)):
+            for i in range(0, len(sequence) - 1):
+                if sequence[i] > sequence[i + 1]:
+                    x = sequence[i + 1]
+                    sequence[i + 1] = sequence[i]
+                    sequence[i] = x
+        return sequence
+    else:
+        raise TypeError('Function must take string or list argument')
+
+
+result = my_sort_function('Hello World Brother!')
+print(result)
+result = my_sort_function([9, 3, 34, 75, 12, 7, 23, 1, 6, 52])
+print(result)
+
 
 # simple example
 def my_function(a, b):
@@ -10,7 +51,7 @@ print(f"1. simple example result is: {my_function(30, 50)}")
 
 # example with *args
 def my_function_2(*args):
-    return sum(args)*0.05
+    return sum(args) * 0.05
 
 
 print(f"2. example with *args result is: {my_function_2(30, 50, 80, 100)}")
